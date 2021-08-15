06a5ebcade3090ed5300de04838a0cef974f7a23
gabrielgamboa <gabrielgamboav19@gmail.com>
2021-08-15 12:08:42 -0600

    Created a client that can check for readings, about to create a server to test it

--- test.py
+++ test.py
@@ -1,22 +1,24 @@
 import socket
-import sys
+import selectors
 
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print ("Socket successfully created")
 except socket.error as err:
     print ("socket creation failed with error %s" %(err))
-
+#add a decent error handling
 port = 5000
 s.connect(('127.0.0.1', port))
-s.setblocking(0)
+
+s_manager = selectors.DefaultSelector()
+s_manager.register(s, selectors.EVENT_READ)
+
+# print(type(s_manager.select(0)))
 while True:
+    events = s_manager.select(None)
+    print('unblocked')
+    if events != []:
+        #handle events
+        key, data = events[0]
 
-    try:
-
-
-        print(str(s.recv(1024)))
-        s.send(bytes("close", 'utf-8'))
-        break
-    except:
-        continue
+        print(key.fileobj.recv(1025))
