2f9b8dc898dfc44fd6315986d6f89b98fe4b3875
gabrielgamboa <gabrielgamboav19@gmail.com>
2021-08-25 15:02:46 -0600

    The server can receive strings from several clients

--- server.c
+++ server.c
@@ -12,6 +12,16 @@
 #include <signal.h>
 #include <poll.h>
 #define BACKLOG 10 //???? why is this needed
+#define PORT "6000"
+
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
 
 int get_listener(char domain[], char port[], struct addrinfo* hints)
 {
@@ -54,10 +64,37 @@ int get_listener(char domain[], char port[], struct addrinfo* hints)
     return sockfd;
 }
 
+void add_fd(struct pollfd *pfds[], int newfd, int *fd_count, int *fd_alloc_size)
+{
+  if (*fd_count == *fd_alloc_size)
+  {
+    *fd_alloc_size *= 2;
+    *pfds = realloc(*pfds, sizeof(**pfds) * (*fd_alloc_size));
+  }
+
+  (*pfds)[*fd_count].fd = newfd;
+  (*pfds)[*fd_count].events = POLLIN;
+  *fd_count += 1;
+}
+
+void del_fd(struct pollfd pfds[], int i, int *fd_count)
+{
+    // Copy the one from the end over this one
+    pfds[i] = pfds[*fd_count-1];
+
+    (*fd_count)--;
+}
+
+
 
 
 int main() {
-  int serverfd;
+  struct sockaddr_storage remoteaddr;
+  socklen_t addrlen;
+  char buf[256];
+  char remoteIP[INET6_ADDRSTRLEN];
+
+  int serverfd, newfd;
   struct addrinfo hints;
   memset(&hints, 0, sizeof hints);
 
@@ -65,7 +102,11 @@ int main() {
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE;
 
-  serverfd = get_listener(NULL, "6000", &hints);
+  int fd_count = 0;
+  int fd_alloc_size = 5;
+  struct pollfd *pfds = malloc(sizeof *pfds * fd_alloc_size);
+
+  serverfd = get_listener(NULL, PORT, &hints);
 
   if (serverfd == -1)
   {
@@ -73,9 +114,72 @@ int main() {
     exit(1);
   }
   printf("Listener on port %d \nWaiting for connections ...", 6000);
-  printf("%d\n", serverfd);
+
+  add_fd(&pfds, serverfd, &fd_count, &fd_alloc_size);
+
+  while (1)
+  {
+    int poll_count = poll(pfds, fd_count, -1);
+
+    if (poll_count == -1)
+    {
+        perror("poll");
+        exit(1);
+    }
+
+    for (int i=0; i<fd_count; i++)
+    {
+      if (pfds[i].revents & POLLIN)
+      {
+        if (pfds[i].fd == serverfd)
+        {
+          addrlen = sizeof remoteaddr;
+          newfd = accept(serverfd,(struct sockaddr *)&remoteaddr,&addrlen);
+
+          if (newfd == -1) {
+              perror("accept");
+          }
+          else
+          {
+            add_fd(&pfds, newfd, &fd_count, &fd_alloc_size);
+
+            printf("pollserver: new connection from %s on "
+                "socket %d\n",inet_ntop(remoteaddr.ss_family,
+                    get_in_addr((struct sockaddr*)&remoteaddr),
+                    remoteIP, INET6_ADDRSTRLEN),newfd);
+          }
+        }//handling new connections
+      else
+      {
+        int nbytes = recv(pfds[i].fd, buf, sizeof buf, 0);
+
+        int sender_fd = pfds[i].fd;
+
+        if (nbytes <= 0)
+        {
+            // Got error or connection closed by client
+            if (nbytes == 0) {
+                // Connection closed
+                printf("pollserver: socket %d hung up\n", sender_fd);
+            }
+            else
+            {
+                perror("recv");
+            }
+
+            close(pfds[i].fd);
+            del_fd(pfds, i, &fd_count);
+      }
+      else //client send a req
+      {
+
+        printf("%s\n", buf);
+      }
 
 
-
-
+      }//check fds that are ready for reading
+    }
   }
+
+  }//infinite loop
+}
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import socket
 import selectors
+import time
 
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -7,18 +8,21 @@ try:
 except socket.error as err:
     print ("socket creation failed with error %s" %(err))
 #add a decent error handling
-port = 5000
+port = 6000
 s.connect(('127.0.0.1', port))
 
 s_manager = selectors.DefaultSelector()
 s_manager.register(s, selectors.EVENT_READ)
-
+start_time = time.time()
 # print(type(s_manager.select(0)))
 while True:
-    events = s_manager.select(None)
-    print('unblocked')
+    if time.time() - start_time > 5.0:
+        s.send("pepino".encode())
+        start_time = time.time()
+
+    events = s_manager.select(0)
+
     if events != []:
         #handle events
         key, data = events[0]
-
-        print(key.fileobj.recv(1025))
+        print(key.fileobj.recv(256))
